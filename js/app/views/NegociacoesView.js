84a7d879984d88cf670ba16b13e3cefc43ae5201
Luiz Romário Filho <luizromariofilho@gmail.com>
2016-12-07 22:35:36 -0300

    Add model mensagem and view

--- js/app/views/NegociacoesView.js
+++ js/app/views/NegociacoesView.js
@@ -1,22 +1,3 @@
-/*  
-	<table class="table table-hover table-bordered">
-  		<thead>
-  			<tr>
-  				<th>DATA</th>
-  				<th>QUANTIDADE</th>
-  				<th>VALOR</th>
-  				<th>VOLUME</th>
-  			</tr>
-  		</thead>
-
-  		<tbody>
-  		</tbody>
-
-  		<tfoot>
-  		</tfoot>
-  	</table> 
-*/
-
 class NegociacoesView {
 
 	constructor(elemento) {
